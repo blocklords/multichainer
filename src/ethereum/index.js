95307b81fc20a6af178be29b6634b323fea16595
Medet Ahmetson Atabayev <admin@blocklords.io>
2020-06-14 02:52:06 +0700

    Delete the loading of smartcontract in index.js

--- src/ethereum/index.js
+++ src/ethereum/index.js
@@ -2,7 +2,6 @@ let Utils = require('./cryptoutils.js');
 let Account = require('./account.js');
 let Provider = require('./provider.js');
 let Contract = require('./contract.js');
-let Smartcontract = require('./smartcontract.js');
 let Config = require('./config.js');
 let Gateway = require('./gateway.js');
 
@@ -10,7 +9,6 @@ module.exports.Utils = Utils;
 module.exports.Account = Account;
 module.exports.Provider = Provider;
 module.exports.Contract = Contract;
-module.exports.Smartcontract = Smartcontract;
 module.exports.Config = Config;
 module.exports.Gateway = Gateway;
 
@@ -19,7 +17,6 @@ module.exports.utils = Utils;
 module.exports.account = Account;
 module.exports.provider = Provider;
 module.exports.contract = Contract;
-module.exports.smartcontract = Smartcontract;
 module.exports.config = Config;
 module.exports.gateway = Gateway;
 module.exports.name = "ethereum";
